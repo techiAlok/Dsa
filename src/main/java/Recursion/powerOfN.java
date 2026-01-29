ba0dc581fafbf1c6765b98f67c2cef149fcba922
techiAlok <159530614+techiAlok@users.noreply.github.com>
2026-01-29 22:47:37 +0530

    Optimize power calculation in pow method
    Refactor power calculation to use optimized method.

--- src/main/java/Recursion/powerOfN.java
+++ src/main/java/Recursion/powerOfN.java
@@ -2,10 +2,19 @@ package Recursion;
 
 public class powerOfN {
     public static int pow(int base, int power) {
+        //O(n)
+        // if(power == 0){
+        //     return 1;
+        // }
+        // return base * pow(base,power-1);
+
+        //optimize way
         if(power == 1){
             return base;
         }
-        return base * pow(base,power-1);
+        int ans = pow(base,power/2) * pow(base,power/2);
+        return ans;
+        
     }
     public static void main(String[] args) {
         System.out.println(pow(2,3));
